4a70149fb73e31bb5212109a201423cf6912e4b2
vkum29 <vkumar76@sapient.com>
2017-04-02 22:26:58 +0530

    break test to validate successful config

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,7 @@
 describe("App calculator", function () {
 	describe("addition", function () {
 		it("Sum of two numbers must be returned", function (done) {
-			var result = add(2, 3);
+			var result = add(2 + 3);
 			if (result === 5) {
 				done();
 			} else {
