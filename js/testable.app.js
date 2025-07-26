a39069eac90de3cdd08fcf1aa129eff6ec8d3aa1
Vijayendra Kumar <vk.developer.in@gmail.com>
2025-07-26 23:31:31 +0530

    html and js file refrences

--- js/testable.app.js
+++ js/testable.app.js
@@ -1,49 +1,78 @@
 /*
-** name: testable.app.js
-** Use: Demo of unit testing with karma, mocha and chai
-*/
+ ** name: testable.app.js
+ ** Use: Demo of unit testing with karma, mocha and chai
+ */
 
-(function () {
-	window.search = function ($target, $results) {
-		this.name = 'search';
-		this.input = $target;
-		this.results = $results;
-		this.responseHandler = this.responseHandler.bind(this);
-		return this;
-	};
-	window.search.prototype.init = function () {
-		var _this = this;
-		_this.input.addEventListener('submit', function (evt) {
-			evt.preventDefault();
-			_this.getResults(_this.input.action, _this.input.action.searchTerm, function (response) { _this.responseHandler.call(_this, response) });
-		});
-		return this;
-	};
-	window.search.prototype.getResults = function (url, term, callback) {
-		$.get(url, term, callback);
-	};
-	window.search.prototype.responseHandler = function (response) {
-		var _this = this;
-		if (response.results && response.results.length > 0) {
-			_this.results.innerHTML = '';
-			response.results.forEach(_this.add.bind(_this));
-		} else {
-			_this.results.innerHTML = '<p class="no-results text-center"> We found no result for entered query.</p>';
-		}
-	};
-	window.search.prototype.add = function (result) {
+window.search = function ($target, $results) {
+  this.name = "search";
+  this.input = $target;
+  this.results = $results;
+  this.responseHandler = this.responseHandler.bind(this);
+  return this;
+};
+window.search.prototype.init = function () {
+  var _this = this;
+  _this.input.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    _this.getResults(
+      _this.input.action,
+      _this.input.action.searchTerm,
+      function (response) {
+        _this.responseHandler.call(_this, response);
+      }
+    );
+  });
+  return this;
+};
+window.search.prototype.getResults = function (url, term, callback) {
+  $.get(url, term, callback);
+};
+window.search.prototype.responseHandler = function (response) {
+  var _this = this;
+  if (response.results && response.results.length > 0) {
+    _this.results.innerHTML = "";
+    response.results.forEach(_this.add.bind(_this));
+  } else {
+    _this.results.innerHTML =
+      '<p class="no-results text-center"> We found no result for entered query.</p>';
+  }
+};
+window.search.prototype.add = function (result) {
+  var details =
+    '<div class="result row"><figure class="product-image col-xs-10 col-xs-offset-1 col-md-offset-0 col-md-4"><img src="" alt="' +
+    result.title +
+    '"/></figure><div class="product-detail col-xs-10 col-md-8 col-xs-offset-1 col-md-offset-0"><h2 class="d-flex justify-content-between">' +
+    result.title +
+    '<a href="javascript:void(0)" class="btn btn-primary btn-like-item"><i class="bi bi-hand-thumbs-up"></i><span class="visually-hidden">Like</span></a></h2><p class="price">Price: ' +
+    result.price +
+    " " +
+    result.unit +
+    '</p><div class="ingredients"><h3>Ingrdients</h3><ul>';
+  details += result.ingredients
+    .map(function (ingredient) {
+      return "<li>" + ingredient + "</li>";
+    })
+    .join("");
+  details +=
+    '</ul></div><h3>Time needed:</h3><ol class="prep-time"><li>Prepration: ' +
+    result.time.preparation +
+    "</li><li>Cooking: " +
+    result.time.cooking +
+    '</li></ol><div class="how-to-bake"><h3>Baking Steps</h3><ul>';
+  details += result.ingredients
+    .map(function (ingredient) {
+      return "<li>" + ingredient + "</li>";
+    })
+    .join("");
+  details += "</ul></div></div></div>";
 
-		var details = '<div class="result row"><figure class="product-image  col-xs-10 col-xs-offset-1 col-md-offset-0 col-md-4"><img src="" alt="' + result.title + '"/></figure><div class="product-detail col-xs-10 col-md-7 col-xs-offset-1"><h2>' + result.title + '<a href="javascript:void(0)" class="btn btn-primary btn-like-item">Like</a></h2><p class="price">Price: ' + result.price + ' ' + result.unit + '</p><div class="ingredients"><h3>Ingrdients</h3><ul>';
-			details += result.ingredients.map(function (ingredient) { return '<li>' + ingredient + '</li>'; }).join('');
-			details += '</ul></div><a href="javascript:void(0)" class="btn btn-primary btn-like-item">Like</a><ol class="prep-time"><li>Prepration: ' + result.time.preparation + '</li><li>Cooking: ' + result.time.cooking + '</li></ol><div class="how-to-bake"><h3>Baking Steps</h3><ul>'
-			details += result.ingredients.map(function (ingredient) { return '<li>' + ingredient + '</li>'; }).join('');
-			details += '</ul></div></div></div>';
+  this.results.insertAdjacentHTML("beforeEnd", details);
+};
 
-			this.results.insertAdjacentHTML('beforeEnd',details);
-	};
-
-	$(function () {
-		var search = new window.search(document.getElementById('searchForm'), document.getElementById('results'));
-		search.init();
-	});
-})();
+$(function () {
+  var search = new window.search(
+    document.getElementById("searchForm"),
+    document.getElementById("results")
+  );
+  search.init();
+});
